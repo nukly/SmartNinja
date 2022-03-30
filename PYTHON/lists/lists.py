a169ec37f9c3095c45b30d2c0f33d31b04dbda54
nukly <56589150+nukly@users.noreply.github.com>
2022-03-30 17:38:33 +0200

    Guessing game updated with lists and score file in json format

---
+++ PYTHON/lists/lists.py
@@ -0,0 +1,29 @@
+import json
+import random
+
+secret = random.randint(1, 30)
+attempts = 0
+
+with open("score_list.json", "r") as score_file:
+    score_list = json.loads(score_file.read())
+    print("Top scores: " + str(score_list))
+
+while True:
+    guess = int(input("Guess the secret number (between 1 and 30): "))
+    attempts += 1
+
+    if guess == secret:
+        score_list.append(attempts)
+        print("You've guessed it - congratulations! It's number " + str(secret))
+        print("Attempts needed: " + str(attempts))
+        with open("score_list.json", "w") as score_file:
+            score_file.write(json.dumps(score_list))
+            score_list.sort()
+            print(score_list[:3])
+        break
+    elif guess > secret:
+        print("Your guess is not correct... try something smaller")
+    elif guess < secret:
+        print("Your guess is not correct... try something bigger")
+
+
