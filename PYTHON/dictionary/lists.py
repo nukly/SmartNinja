be0ff2569ef93695eeaee7f8cfb83b1fb12f19ea
nukly <56589150+nukly@users.noreply.github.com>
2022-03-31 15:14:01 +0200

    Changes in dictionary and some css in bootstrap app

--- PYTHON/dictionary/lists.py
+++ PYTHON/dictionary/lists.py
@@ -3,12 +3,12 @@ import random
 import datetime
 
 current_time = datetime.datetime.now()
-print(current_time)
 
 secret = random.randint(1, 30)
 attempts = 0
+username = input("Enter name to play guessing game: ")
 
-score_data = {"attempts": attempts, "date": datetime.datetime.now}
+score_data = {"username": username, "attempts": attempts, "date": datetime.datetime.now, "secret_number": secret}
 
 with open("score_list.json", "r") as score_file:
     score_list = json.loads(score_file.read())
@@ -19,16 +19,16 @@ while True:
     attempts += 1
 
     if guess == secret:
-        score_list.append({"attempts": attempts, "date": str(datetime.datetime.now())})
+        score_list.append({"username:": username, "attempts": attempts, "date": str(datetime.datetime.now()),
+                           "secret_number": secret})
         print("You've guessed it - congratulations! It's number " + str(secret))
         with open("score_list.json", "w") as score_file:
             score_file.write(json.dumps(score_list))
             for score_dict in score_list:
-                print(str(score_dict["attempts"]) + " attempts, date: " + score_dict.get("date"))
+                print(str("username:" + score_dict["username"] + ", " + score_dict["attempts"] +
+                          "attempts, date: " + score_dict["date"] + score_dict["secret_number"]))
         break
     elif guess > secret:
         print("Your guess is not correct... try something smaller")
     elif guess < secret:
         print("Your guess is not correct... try something bigger")
-
-
